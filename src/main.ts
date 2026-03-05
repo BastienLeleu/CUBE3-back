658bd0da66d5fcfad53c93bb3a7b303f220b2af4
Bastien Leleu <bastienleleu6@gmail.com>
2026-03-05 09:56:27 +0100

    lint error fix

--- src/main.ts
+++ src/main.ts
@@ -2,22 +2,24 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
 
   // Active Helmet pour corriger les failles d'entêtes HTTP (ZAP Scan)
-  app.use(helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        scriptSrc: ["'self'", "'unsafe-inline'"],
-        styleSrc: ["'self'", "'unsafe-inline'"],
-        imgSrc: ["'self'", "data:", "https:"],
+  app.use(
+    helmet({
+      contentSecurityPolicy: {
+        directives: {
+          defaultSrc: ["'self'"],
+          scriptSrc: ["'self'", "'unsafe-inline'"],
+          styleSrc: ["'self'", "'unsafe-inline'"],
+          imgSrc: ["'self'", 'data:', 'https:'],
+        },
       },
-    },
-    crossOriginEmbedderPolicy: true, // Aide contre Spectre
-    referrerPolicy: { policy: 'same-origin' },
-  }));
+      crossOriginEmbedderPolicy: true, // Aide contre Spectre
+      referrerPolicy: { policy: 'same-origin' },
+    }),
+  );
 
   // Autorise le CORS si ton front est sur un autre port
   app.enableCors();
