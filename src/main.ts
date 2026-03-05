44168c7ac9cef882b2591bf01abfc1cc4a23f3aa
Bastien Leleu <bastienleleu6@gmail.com>
2026-03-05 09:45:06 +0100

    main ts update helmet conf

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,27 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
 
-async function bootstrap(): Promise<void>{
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.use(helmet());
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Active Helmet pour corriger les failles d'entêtes HTTP (ZAP Scan)
+  app.use(helmet({
+    contentSecurityPolicy: {
+      directives: {
+        defaultSrc: ["'self'"],
+        scriptSrc: ["'self'", "'unsafe-inline'"],
+        styleSrc: ["'self'", "'unsafe-inline'"],
+        imgSrc: ["'self'", "data:", "https:"],
+      },
+    },
+    crossOriginEmbedderPolicy: true, // Aide contre Spectre
+    referrerPolicy: { policy: 'same-origin' },
+  }));
+
+  // Autorise le CORS si ton front est sur un autre port
+  app.enableCors();
+
+  await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
